6791c18b7a88d7a0402e1c0fce9a6c2ea40a9f9a
YSH116 <steven0909509363@gapp.nthu.edu.tw>
2021-04-07 03:05:35 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,82 +3,90 @@
 
 AnalogOut aout(PA_4);
 AnalogIn ain(D11);
-InterruptIn down(D8);
+InterruptIn dwn(D8);
 InterruptIn sel(D9);
 InterruptIn up(D5);
 uLCD_4DGL uLCD(D1, D0, D2);
 
 float rates;
 float T = 0.24;
+int sample_ = 240*50 / T / 500;
+int ratio;
 Thread t;
+float ADCdata[500];
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 
 void add() { 
-    rates = rates * 2.0;
+    rates *= 2.0;
     uLCD.locate(0,2);
     uLCD.printf("%f", rates);
 }
 
-void minus() {
-    rates /= 2;
+void down() {
+    rates /= 2.0;
     uLCD.locate(0,2);
     uLCD.printf("%f", rates);
 }
 
-/*void nex() {
-    queue.call(generate);
-}*/
-
-/*void generate() {
+void gener() {
     int ratio = 1;
-    
-    while (1) {
-        if (rates == 1) {
-            if (ratio == 240) ratio = 0; // generate triangular wave & S = 2 
-            else if (ratio <= 80) aout = ratio / 80.0 / 1.1;                
-            else if (ratio > 160) aout = (240 - ratio) / 80.0 / 1.1;
-            else aout = ratio / 1.1;
-            ratio++;
-        }
-        if (rates == 1/2) {
-            if (ratio == 240) ratio = 0; // generate triangular wave & S = 2 
-            else if (ratio <= 40) aout = ratio / 40.0 / 1.1;                
-            else if (ratio > 200) aout = (240 - ratio) / 200.0 / 1.1;
-            else aout = ratio / 1.1;
-            ratio++;
-        }
-        if (rates == 1/4) {
-            if (ratio == 240) ratio = 0; // generate triangular wave & S = 2 
-            else if (ratio <= 20) aout = ratio / 20.0 / 1.1;                
-            else if (ratio > 220) aout = (240 - ratio) / 220.0 / 1.1;
-            else aout = ratio / 1.1;
-            ratio++;
-        }
-        if (rates == 1/8) {
-            if (ratio == 240) ratio = 0; // generate triangular wave & S = 2 
-            else if (ratio <= 10) aout = ratio / 10.0 / 1.1;                
-            else if (ratio > 230) aout = (240 - ratio) / 230.0 / 1.1;
-            else aout = ratio / 1.1;
-            ratio++;
-        }
-    }
-}*/
 
-/*void sample() {
-    if (sample_pt >= 500) { // sample 
+    while (rates == 1) {
+        if (ratio == 240*50) ratio = 0;  
+        if (ratio <= 80*50) aout = ratio / 80.0 / 1.1 / 50;                
+        if (ratio > 160*50) aout = (240*50 - ratio) / 80.0 / 1.1 / 50;
+        if (ratio > 80*50 && ratio <= 160*50) aout = 1 / 1.1;
+        ratio++;
+        wait_us(T/240);
+    }
+    while (rates == 0.5) {
+        if (ratio == 240*50) ratio = 0;  
+        if (ratio <= 40*50) aout = ratio / 40.0 / 1.1 / 50;                
+        if (ratio > 200*50) aout = (240*50 - ratio) / 200.0 / 1.1 / 50;
+        if (ratio > 40*50 && ratio <= 200*50) aout = 1/1.1;
+        ratio++;
+        wait_us(T/240);
+    }
+    while (rates == 0.25) {
+        if (ratio == 240*50) ratio = 0;  
+        if (ratio <= 20*50) aout = ratio / 20.0 / 1.1 / 50;                
+        if (ratio > 220*50) aout = (240*50 - ratio) / 220.0 / 1.1 / 50;
+        if (ratio > 20*50 && ratio <= 220*50) aout = 1/1.1;
+        ratio++;
+        wait_us(T/240);
+    }
+    while (rates == 0.125) {
+        if (ratio == 240*50) ratio = 0; 
+        if (ratio <= 10*50) aout = ratio / 10.0 / 1.1 / 50;                
+        if (ratio > 230*50) aout = (240*50 - ratio) / 230.0 / 1.1 / 50;
+        if (ratio > 10*50 && ratio <= 230*50) aout = 1 / 1.1;
+        ratio++;  
+        wait_us(T/240);
+    }
+   // queue.call(&sample);
+}
+
+void sample() {
+    int sample_pt = 1;
+    int count_loop = 1;
+
+    while (1) {
+        if (sample_pt >= 500) { // sample 
             sample_pt = 0; 
             count_loop = 0;
         } 
-        else if (count_loop % sample == 0) {
+        else if (count_loop % sample_ == 0) {
             ADCdata[sample_pt] = ain;
             sample_pt++;
         }
-        count_loop++;
-        wait_us(T/100);
-}*/
+    count_loop++;
+    }
+}
 
 int main()
 {   
+   int l;
+
    t.start(callback(&queue, &EventQueue::dispatch_forever));
    rates = 0.125; 
    uLCD.text_width(2); 
@@ -86,9 +94,9 @@ int main()
    uLCD.color(RED);
    uLCD.locate(0,2);
    uLCD.printf("%f", rates); 
-   
-    up.rise(&add); // attach the address of the flip function to the rising edge
-   //down.rise(&minus);
-  // sel.rise(&nex);
-   
+   up.rise(queue.event(add)); // attach the address of the flip function to the rising edge
+   dwn.rise(queue.event(down));
+   sel.rise(queue.event(gener));
+  // for (l = 0; l < 500; l++)
+    //   printf("%f\r\n", ADCdata[l]);
 }
